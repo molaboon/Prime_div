54e802d0ef5338dfc730e94c7b6db8502ad4e5fa
molaboon <how88889@gmail.com>
2023-06-17 15:14:58 +0800

    done

--- prime.cpp
+++ prime.cpp
@@ -1,35 +1,42 @@
 #include <stdio.h>
 #include <math.h>
 
-int prime_div(int start, int n, int *d, int *cnt)
+int cnt = 0;
+int prime_div(int start, int n, int d[])
 {
     int limit = 0;
     limit = sqrt(n);
-    for( ; start < limit; start++)
+    for(start = 2 ; start <= limit; start++)
     {
-        if(n % start == 0) {
-            n = n % start;
-            d[*cnt] = start;
-            *cnt += 1;
-            return 1 + prime_div(start, n, d, cnt);
+        if(n % start == 0) 
+	{
+            n = n / start;
+            d[cnt] = start;
+            cnt++;
+	   
+            return 1 + prime_div(start, n, d);
         }
+
     }
-    if (cnt == 0)
-    {
-        d[*cnt] == n;
-    }
+    d[cnt] = n;
+    
     return 1;
 }
  
 int main(void)
 {   
-    int n, n_div, cnt = 0;
-    int div [128];
+    int n, n_div;
+    int div [128] = {0};
 
     scanf("%d", &n);
-    n_div = prime_div(2, n, div, &cnt);
+    n_div = prime_div(2, n, div);
+    printf("\n");
 
-    printf("%d", div);
+    for (int i = 0; div[i] != 0; i++)
+    {
+    	printf("%d ", div[i]);
+    }
+    printf("\n");
 
     return 0;
     
